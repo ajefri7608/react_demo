768c991c2004ecbfbbd2d7de4e7adf917b2d7164
jefriSaeTem <jefri.sae-tem@varmeego.com>
2023-02-17 23:26:40 -0500

    product search bar

---
+++ src/view/components/searchBar/SearchBar.tsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./SearchBar.scss";
+
+type Param = {
+  textChangeCallBack: (text: string) => void;
+  currentText: string;
+};
+export const SearchBar = (param: Param) => {
+  return (
+    <div className="searchBarContainer">
+      <span className="searchBarTitle">Product Name:</span>
+      <div className="searchBarGrp">
+        <img src={require("assets/icon/search.png")} className="searchIcon" />
+        <input
+          className="searchTextInput"
+          style={{ outline: "none", border: "none" }}
+          type="text"
+          value={param.currentText}
+          onChange={(event) => param.textChangeCallBack(event.target.value)}
+          placeholder="Search in here"
+        ></input>
+      </div>
+    </div>
+  );
+};
--- src/view/productSearch/component/productSearchBar/ProductSearchBar.tsx
+++ src/view/productSearch/component/productSearchBar/ProductSearchBar.tsx
@@ -1,10 +1,16 @@
 import React, { useState } from "react";
+import { SearchBar } from "view/components/searchBar/SearchBar";
 import { SwitchBtn } from "view/components/switchBtn/SwitchBtn";
 
 export const ProductSearchBar = () => {
   const [switchBtn, setSwitchBtn] = useState(false);
+  const [text, setText] = useState("");
   return (
     <div className="productSearchBarContainer">
+      <SearchBar
+        currentText={text}
+        textChangeCallBack={(text) => setText(text)}
+      />
       <SwitchBtn
         swichCallBack={(isOn) => {
           console.log("switch", isOn);
